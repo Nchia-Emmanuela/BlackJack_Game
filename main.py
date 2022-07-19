7972011edb33f78640d03e461b7b3ef49ec0c584
unknown <ekeinchaiemmanuela@gmail.com>
2022-07-19 18:23:09 +0100

    compare function created

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from arts import logo
 def random_card():
     """Returns a random card"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(card)
+    card = random.choice(cards)
     return card
 
 def calc_score(cards):
@@ -14,7 +14,23 @@ def calc_score(cards):
     if 11 in cards and sum(cards ) > 21:
         cards.remove(11)
         cards.append(1)
-    sum(cards)
+    return sum(cards)
+
+def compare(players_score, computers_score):
+    if players_score == computers_score:
+        return "Draw"
+    elif computers_score == 0:
+        return "Lose, opponent has BlackJack 😱"
+    elif players_score == 0:
+        return "Win, with a BlackJack 😎"
+    elif players_score > 21:
+        return "You went over, you lose 😥"
+    elif computers_score > 21:
+        return "Opponent went over, you win 😀"
+    elif players_score > computers_score:
+        return "You win😃😀"
+    else:
+        return "You lose😥"
 
 player_cards = []
 computer_card = []
@@ -44,3 +60,5 @@ while not is_game_over:
 while computers_score != 0 and computers_score < 17:
     computer_card.append(random_card())
     computers_score = calc_score(computer_card)
+
+print(compare(players_score, computers_score))
