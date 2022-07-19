0880e8d9f4787bc74ead70ff7ef2224f6d3db8a4
unknown <ekeinchaiemmanuela@gmail.com>
2022-07-19 17:19:18 +0100

    refactor

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import random
 from arts import logo
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 def random_card():
-    selected_cards =[]
-    for i in range(0, 2):
-        random_point= random.randint(0, len(cards)-1)
-        picked_card = cards[random_point]
-        selected_cards.append(picked_card)
-    return selected_cards
+    """Returns a random card"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(card)
+    return card
 
-players_card = random_card()
-score = players_card[0] + players_card[1]
-print(f"your current cards: {players_card}, current score: {score}")
+player_cards = []
+computer_card = []
 
-computers_card = random_card()
-print(f"Computer's first card: {computers_card[0]}")
+for i in range(2):
+    player_cards.append(random_card())
+    computer_card.append(random_card())
