4cece7ff4fab7c00895cf7924a9996fd7111fdb5
unknown <ekeinchaiemmanuela@gmail.com>
2022-07-19 18:42:04 +0100

    clear function written to clear console

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
+import os
 from arts import logo
+
+def clear(): 
+    os.system('cls')
 
 def random_card():
     """Returns a random card"""
@@ -31,34 +35,41 @@ def compare(players_score, computers_score):
         return "You win😃😀"
     else:
         return "You lose😥"
+def play_game():
+    print(logo)
+    player_cards = []
+    computer_card = []
 
-player_cards = []
-computer_card = []
+    is_game_over = False
 
-is_game_over = False
-
-for i in range(2):
-    player_cards.append(random_card())
-    computer_card.append(random_card())
+    for i in range(2):
+        player_cards.append(random_card())
+        computer_card.append(random_card())
 
 
-while not is_game_over:
-    players_score = calc_score(player_cards)
-    computers_score = calc_score(computer_card)
-    print(f"your cards: {player_cards}, current score: {players_score}")
-    print(f"Computer's first card: {computer_card[0]}, current score: {computers_score}")
+    while not is_game_over:
+        players_score = calc_score(player_cards)
+        computers_score = calc_score(computer_card)
+        print(f"your cards: {player_cards}, current score: {players_score}")
+        print(f"Computer's first card: {computer_card[0]}, current score: {computers_score}")
 
-    if players_score == 0 or computers_score == 0 or players_score > 21:
-        is_game_over = True
-    else:
-        take_card = input("type y to take another card type n to pass: ")
-        if take_card == "y":
-            player_cards.append(random_card())
-        else:
+        if players_score == 0 or computers_score == 0 or players_score > 21:
             is_game_over = True
+        else:
+            take_card = input("type y to take another card type n to pass: ")
+            if take_card == "y":
+                player_cards.append(random_card())
+            else:
+                is_game_over = True
 
-while computers_score != 0 and computers_score < 17:
-    computer_card.append(random_card())
-    computers_score = calc_score(computer_card)
+    while computers_score != 0 and computers_score < 17:
+        computer_card.append(random_card())
+        computers_score = calc_score(computer_card)
 
-print(compare(players_score, computers_score))
+    print(f"your final hand : {player_cards}, final score: {players_score}")
+    print(f"Computers final hand : {computer_card}, final score: {computers_score}")
+    print(compare(players_score, computers_score))
+
+while input("Do you want to play the BlackJack game? type y or n: ") == "y":
+    clear()
+    play_game()
